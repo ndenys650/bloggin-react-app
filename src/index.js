3e9320fe5ab32f5ee8eee12425ec26e62f4eb0e8
Nate Denys <nate_denys@yahoo.com>
2017-09-25 20:15:52 -0700

    Setup first route with first component
    removed old unnecessary components and hardcoded Index Route to confirm app is functioning correctly so far, except for small bug that will be fixed in next commit

--- src/index.js
+++ src/index.js
@@ -2,30 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+// import Router functionality tools
 import { BrowserRouter, Route } from 'react-router-dom';
 
-
-import App from './components/app';
 import reducers from './reducers';
+// import PostsIndex Component
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-	render() { return <div>Hello!</div> }
-}
-
-class Goodbye extends React.Component {
-	render() { return <div>Goodbye!</div> }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     	<div>
-    		Header
-    		<Route path="/hello" component={Hello} />
-    		<Route path="/goodbye" component={Goodbye} />
-    	</div>
+    		{/* Create first route and use first imported component */}
+    		<Route path="/" component={PostsIndex} />
+       	</div>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
