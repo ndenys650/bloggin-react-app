8c9a816475a9ff13f3578ba2d42a3d0e6d434f8d
Nate Denys <nate_denys@yahoo.com>
2017-09-26 07:06:47 -0700

    Create first field input for a form
    defined a component prop return jsx that communicates with redux what is entered into the input tag

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+// reduxForm knows 
+
 
 class PostsNew extends Component {
-	render() {
+	// field object contains event handlers we need to wire up to jsx we need to return
+	renderTitleField(field) {
 		return (
 			<div>
-				PostsNew!
+				<input
+					//  ... says take the object with all its properties 
+					// to be communicated as props to the input tag 
+					{...field.input}
+				/>
 			</div>
+		);
+	}
+
+	render() {
+		return (
+			<form>
+				<Field
+					name="title"
+					component={this.renderTitleField}
+				/>
+			</form>
 		);
 	}
 }
 
-export default PostsNew;
+// use redux form to wrap the PostsNew Component
+// allowing for clear communication between reducer and this component
+// give form property to specify a specific state this component will use
+export default reduxForm({
+	// 'form' sets unique string to name this form
+	form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
 	// posts piece of state
-	posts: PostsReducer
+	posts: PostsReducer,
+	// wire in formReducer and assign to key of form
+	form: formReducer
 });
 
 export default rootReducer;
